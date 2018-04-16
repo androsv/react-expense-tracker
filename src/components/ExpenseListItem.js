8fb90090af45e4f829c1bbbaa9c6f8e1a7eccb52
androsv <airuser1993@gmail.com>
2018-04-16 22:52:23 +0530

    added currency and createdAt format

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral'
 
 const ExpenseListItem = ({dispatch,id,description, amount, createdAt})=> (
 
     <div>
     <NavLink to={`/edit/${id}`}><h3>{description}</h3></NavLink>
-    <h5>Amount - {amount} Created at - {createdAt}</h5>
+    <h5>{"₹ " }{numeral(amount).format('0,0.00')} { moment(createdAt).format('MMMM Do, YYYY')}</h5>
 
     </div>
 );
