e03f933ba9342e0faad8002901d6951a33ca35a6
androsv <airuser1993@gmail.com>
2018-04-17 00:43:59 +0530

    added expense summary to dashboard

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ExpenseList from './ExpenseList'
 import ExpenseListFilter from './ExpenseListFilter'
+import ExpenseSummary from './ExpenseSummary';
 
 const ExpenseDashboardPage = ()=> {
     return(
     <div>
+    <ExpenseSummary/>
     <ExpenseListFilter/>
     <ExpenseList/>
     </div>
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
 import filterExpense from '../selectors/expenses'
+import expenseTotal from '../selectors/expense-total';
+import numeral from 'numeral';
 const ExpenseList = (props)=>(
 
     <div>
     <h3>Expense List</h3>
+    
     {props.expenses.map((expense)=>{
         return <ExpenseListItem {...expense}/>
     })}
