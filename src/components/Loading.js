59d0cbf2bc5c4f92f60c2cdab9b1ada32ea3658a
androsv <airuser1993@gmail.com>
2018-04-28 16:22:09 +0530

    fixed loader on mobile

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,7 +2,7 @@ import ReactLoading from 'react-loading';
 import React from 'react';
 
  const Loading = ({ type, color }) => (
-	 <div className="loader">
+    <div className="loader ">
 	<ReactLoading  type={"bars"} color={"blue"} height={667} width={375} />
 	</div>
 );
